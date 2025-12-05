722f9fb449af77e40995022e6b50ddddc3d7a8c7
chan <gim2785@gmail.com>
2025-12-06 01:07:39 +0900

    test1

--- threads/palloc.c
+++ threads/palloc.c
@@ -10,7 +10,7 @@
 #include "threads/loader.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
-
+//202201505
 /* Page allocator.  Hands out memory in page-size (or
    page-multiple) chunks.  See malloc.h for an allocator that
    hands out smaller chunks.
